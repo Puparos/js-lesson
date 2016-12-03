192aba7afbd3484e4f3793536b81e75545af208f
puparos <pavluk2004@ukr.net>
2016-12-03 16:37:09 +0200

    01.12.16

--- js/jqtest.js
+++ js/jqtest.js
@@ -1,20 +1,37 @@
 $(document).ready(function () {
-   console.log('ready')
+    console.log('ready');
+
 });
+
 function login() {
-    var username1=$("#firstInput").val();//by id
-    var username2=$("input[name='username']");
-    var form=$("form[name='userForm']").find(username2);
+    var username1 = $("#firstInput").val(); //get value of 1-st input
+    //by id
+    var username2 = $("input[name='username']");
+    var form = $("form[name='userForm']").find(username2);
     form.val(username1);
-}
-function showPassword() {
-    var inp=$("input[name='userpassword']").attr("type");
-    var type=inp.attr("type");
-    var btn=$("button[name='showPwd']");
-    if (type ==="password") {
-        inp.attr("type", "text");
-        btn.text("скрыть пароль");
-    }
-    else
 
 }
+
+function showPwd() {
+    var inp = $("input[name='userpassword']");
+    var type = inp.attr("type");
+    var btn = $("button[name='showpwd']");
+    if (type === "password") {
+        inp.attr("type", "text");
+        btn.text("Скрыть пароль");
+    }
+    else {
+        inp.attr("type", "password");
+        btn.text("Показать пароль");
+    }
+    console.log(inp);
+    // .attr("type")
+}
+
+// .append() - http://ruseller.com/jquery.php?id=28
+// .before() - http://ruseller.com/jquery.php?id=35
+// .addClass() - http://ruseller.com/jquery.php?id=19
+// .removeClass() - http://ruseller.com/jquery.php?id=21
+// .hasClass() - http://ruseller.com/jquery.php?id=20
+// .attr() - http://ruseller.com/jquery.php?id=43
+// .each() - http://jquerybook.ru/api/.each()-fn59.html
